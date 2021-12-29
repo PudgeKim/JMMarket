59c765a368217b6f4a4d80c373dd079fc5c0bd58
PudgeKim <zxaq15@gmail.com>
2021-12-29 21:09:20 +0900

    add ABCToken contract

---
+++ smartcontract/ABCToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ABCToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("ABCToken", "ABC") {
+        _mint(msg.sender, initialSupply);
+    }
+}
